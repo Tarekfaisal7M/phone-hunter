2ae89cb837f6649dce08413c1e07b6ae0f3f7cd3
Tarekfaisal7M <faisal71106@gmail.comgit config --global user.name sajibgit config --global user.name Tarekfaisal7M git config --global user.email faisal71106@gmail.com>
2024-02-29 19:14:40 +0600

    all phone added

---
+++ js/phone.js
@@ -0,0 +1,31 @@
+const loadPhone = async() => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=iphone`);
+    const data = await res.json();
+    const phones = data.data;
+    // console.log(phones);
+    displayPhones(phones);
+}
+
+const displayPhones = phones => {
+    // console.log(phones);
+    const phoneContainer = document.getElementById(`phone-container`);
+    phones.forEach(phone =>{
+        console.log(phone);
+        const phoneCard = document.createElement(`div`);
+        phoneCard.classList =`card w-96 bg-base-100 shadow-xl`
+        phoneCard.innerHTML = `
+         <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                            alt="Shoes" /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div class="card-actions justify-end">
+                            <button class="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+        `;
+        phoneContainer.appendChild(phoneCard);
+    })       
+}
+
+loadPhone();  
