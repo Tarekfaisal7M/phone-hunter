d4ea8d32516553eb1d41073e04029eda033f1efd
Tarekfaisal7M <faisal71106@gmail.comgit config --global user.name sajibgit config --global user.name Tarekfaisal7M git config --global user.email faisal71106@gmail.com>
2024-03-01 02:34:57 +0600

    search option added

--- js/phone.js
+++ js/phone.js
@@ -1,5 +1,5 @@
-const loadPhone = async() => {
-    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=iphone`);
+const loadPhone = async(searchText) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
     // console.log(phones);
@@ -9,15 +9,32 @@ const loadPhone = async() => {
 const displayPhones = phones => {
     // console.log(phones);
     const phoneContainer = document.getElementById(`phone-container`);
+    phoneContainer.textContent = ``;
+
+    // display showall button.......
+    const showAllContainer = document.getElementById(`show-all-container`);
+    if(phones.length > 12){
+        showAllContainer.classList.remove(`hidden`);
+    }
+    else{
+        showAllContainer.classList.add(`hidden`);
+    }
+
+
+
+    // display only 10 phones
+    phones = phones.slice(0, 12);
+
+
     phones.forEach(phone =>{
         console.log(phone);
         const phoneCard = document.createElement(`div`);
-        phoneCard.classList =`card w-96 bg-base-100 shadow-xl`
+        phoneCard.classList =`card bg-base-100  shadow-xl`
         phoneCard.innerHTML = `
-         <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+         <figure><img src="${phone.image}"
                             alt="Shoes" /></figure>
                     <div class="card-body">
-                        <h2 class="card-title">Shoes!</h2>
+                        <h2 class="card-title">${phone.phone_name}</h2>
                         <p>If a dog chews shoes whose shoes does he choose?</p>
                         <div class="card-actions justify-end">
                             <button class="btn btn-primary">Buy Now</button>
@@ -28,4 +45,13 @@ const displayPhones = phones => {
     })       
 }
 
-loadPhone();  
+
+const  handleSearch = () =>{
+    const searchField = document.getElementById(`search-field`);
+    const searchText = searchField.value;
+    console.log(searchText);
+    loadPhone(searchText);
+     
+}
+
+// loadPhone();
